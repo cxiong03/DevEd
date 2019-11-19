538655230ab567073b59690daab0fecefc56ffbe
Chang Xiong <cnxiong03@gmail.com>
2019-11-18 19:32:02 -0500

    completed deved rest api video

--- app.js
+++ app.js
@@ -1,11 +1,20 @@
 const express = require('express'); // We import the package
-
 const app = express(); // We execute it
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+require('dotenv/config');
+
+app.use(bodyParser.json());
 
 // MIDDLEWARES - A FUNCTION THAT EXECUTES WHEN A SPECIFIC WHEN ROUTES ARE BEING HIT
 // app.use('/posts', () => {
 //     console.log('This is a middleware running');
 // });
+
+// IMPORT ROUTES
+const postsRoute = require('./routes/posts');
+
+app.use('/posts', postsRoute); //MIDDLEWARE 
 
 
 // ROUTES
@@ -13,9 +22,9 @@ app.get('/', (req, res) => {
     res.send('Welcome Home');
 });
 
-app.get('/posts', (req, res) => {
-    res.send('We are on posts');
-});
+// Connect to DB
+mongoose.connect(process.env.DB_CONNECTION, { useNewUrlParser: true, useUnifiedTopology: true }, () =>          console.log('Connected to DB!')
+);
 
 // HOW TO START LISTENING TO THE SERVER
 app.listen(3001)
